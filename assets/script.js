5577f430e4a57d07177be346eb7e843add764dc7
JacksonMosh <jaxonmosh@gmail.com>
2023-08-08 01:06:42 +0300

    Changes to be committed: modified:   assets/image/bg/bg.jpg modified:   assets/image/bg/fire-2.png modified:   assets/script.js modified:   assets/style.css modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,80 @@
 let MenuBars = document.querySelector('.menu-bars');
 const aside = document.querySelector('aside');
+const body = document.querySelector('body');
 
 MenuBars.addEventListener('click', () => {
     aside.classList.toggle('aside-open');
+    body.classList.toggle('lock');
 });
 
 
-const distortion = document.querySelectorAll('.distortion-text span');
+const distortion = document.querySelectorAll('.distortion-text h1 span');
+const distortionH2 = document.querySelectorAll('h2 span');
+
+
+setInterval(blur, 1050);
+setInterval(unblur, 300);
+
+
+function blur() {
+    distortionH2.forEach((element, i) => {
+        setTimeout(() => {
+            element.style.cssText = 'filter: blur(3px);';
+        }, 50 * (i + 1));
+    })
+};
+
+function unblur() {
+    distortionH2.forEach((element, i) => {
+        setTimeout(() => {
+            element.style.cssText = 'filter: blur(0px);';
+        }, 50 * (i + 1));
+        return
+    })
+}
+
+
+
+
+
+
+
+
+// кожну секунду має блимати літера
+// отримуємо літери
+// робимо функцію яка з інтервалом надає кожній літері властивість
+// виконуємо цю функцію з інтервалом
+// 
+// 
+// 
+
+
+// const blink = function () {
+//     let timer = new Date();
+//     let interval = timer.getSeconds();
+//     while (interval) {
+//         distortionH2.forEach(element => {
+//             element.style.cssText = 'color: red;'
+//         });
+//     }   
+// };
+
 
 let numX = Math.round(Math.random() * 10);
 
 function getRandomValue() {
-    const transformValues = ['rotate(10deg)', 'rotate(-8deg)', 'skew(30deg, 10deg)', 'scale(0.8, 1)', 'scale(0.9, 1.1)', 'scale(0.9, 0.7)', 'scale(0.7, 1.1)', 'scale(1.2, 0.6)', 'scale(0.9, 0.7)', 'skew(20deg, 40deg)', 'skew(10deg, -10deg);']
+    const transformValues = ['rotate(10deg)', 'rotate(-8deg)', 'skew(30deg, 10deg)',
+        'scale(0.8, 1)', 'scale(0.9, 1.1)', 'scale(0.9, 0.7)', 'scale(0.7, 1.1)',
+        'scale(1.2, 0.6)', 'scale(0.9, 0.7)', 'skew(20deg, 40deg)', 'skew(10deg, -10deg);']
+
     const transformValue = transformValues[Math.floor(Math.random() * transformValues.length)];
-    const values = ['text-shadow: 0 0 20px #fff;', 'text-shadow: 0 10px 20px #ffffffaa;', `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`];
-    let value = values[Math.floor(Math.random() * values.length)] + values[Math.floor(Math.random() * values.length)];
+
+    const values = ['text-shadow: 0 0 20px #fff;', 'text-shadow: 0 10px 20px #ffffffaa;',
+        `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`,
+        `transform: ${transformValue};`, `transform: ${transformValue};`, `transform: ${transformValue};`];
+
+    let value = values[Math.floor(Math.random() * values.length)] +
+        values[Math.floor(Math.random() * values.length)];
     return value;
 }
 distortion.forEach(e => {
@@ -32,19 +92,28 @@ distortion.forEach(e => {
 });
 
 
+
+
 const flameScene = document.querySelectorAll('.flame');
 
-
-
 window.addEventListener("mousemove", function (e) {
+    let htmlWidth = document.documentElement.offsetWidth;
+    let htmlHeight = document.documentElement.clientHeight;
+    let offsetWidth = (e.clientX) - (htmlWidth / 2);
+    let offsetHeight = (e.clientY) - (htmlHeight);
+
+    let offsetWidthBg = ((e.clientX) - (htmlWidth / 2));
+    let offsetHeightBg = ((e.clientY) - (htmlHeight / 2));
+
+    let bg = document.querySelector('.scene__bg');
+    bg.style.cssText = 'right: ' + (offsetWidth * 0.02) + 'px; bottom:' + -(offsetHeight * 0.04) + 'px;';
+    let bgImg = document.querySelector('.scene__bg img');
+    bgImg.style.cssText = `transform: rotateX(${(offsetHeightBg / 700)}deg) rotateY(${offsetWidthBg / 800}deg);`;
+    console.log(bg.style.cssText);
     flameScene.forEach(() => {
-        let htmlWidth = document.documentElement.offsetWidth;
-        let htmlHeight = document.documentElement.offsetHeight;
-        let offsetWidth = (e.clientX) - (htmlWidth / 2);
-        let offsetHeight = (e.clientY) - (htmlHeight / 2)
-        flameScene[0].style.cssText = 'right: ' + -(offsetWidth * 0.06) + 'px; bottom:' + ((offsetHeight * 0.015)) + 'px;';
-        flameScene[1].style.cssText = 'right: ' + -(offsetWidth * 0.09) + 'px; bottom:' + (offsetHeight * 0.030) + 'px;';
-        flameScene[2].style.cssText = 'right: ' + -(offsetWidth * 0.12) + 'px; bottom:' + (offsetHeight * 0.020) + 'px;';
+        flameScene[0].style.cssText = 'right: ' + -(offsetWidth * 0.04) + 'px; bottom:' + -(offsetHeight * 0.02) + 'px;';
+        flameScene[1].style.cssText = 'right: ' + -(offsetWidth * 0.06) + 'px; bottom:' + (offsetHeight * 0.04) + 'px;';
+        flameScene[2].style.cssText = 'right: ' + -(offsetWidth * 0.10) + 'px; bottom:' + (offsetHeight * 0.08) + 'px;';
     });
 });
 
